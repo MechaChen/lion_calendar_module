450e26290038e3b6a54e52da590d5a3fddaf8511
Benson Chen <tumlivein@gmail.com>
2019-09-09 15:40:36 +0800

    環境設定

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "reset.css/reset.css";
+import "./styles/style.scss";
+
+const template = <h1>Hello World</h1>;
+
+ReactDOM.render(template, document.getElementById("app"));
